5d2f79909bfe9a43dad2fd9fe311231a851af483
nenadstojkovic <neske.stojkovic@gmail.com>
2021-01-27 22:23:24 +0100

    Final product

---
+++ src/components/Products/styles.js
@@ -0,0 +1,15 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+    toolbar: theme.mixins.toolbar,
+    content: {
+      flexGrow: 1,
+      // backgroundColor: theme.palette.background.default,
+      backgroundColor: '#d8e1ec',
+      padding: theme.spacing(3),
+    },
+    root: {
+      flexGrow: 1,
+    },
+  })
+);
